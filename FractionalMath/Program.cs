af63f139c9a2ee2589f2c2206bb828ff0feb8924
Quinn <Fyzxs@users.noreply.github.com>
2020-01-21 18:01:38 -0800

    Inital Commit with projects created

---
+++ FractionalMath/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FractionalMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FractionalMathLib.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FractionalMathLib.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
