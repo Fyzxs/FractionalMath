10dcab6b89cf7126bda984e37b3c8ea578b758f5
Quinn <Fyzxs@users.noreply.github.com>
2020-01-21 18:32:58 -0800

    Pulled in a decimal to fraction algorithm and a couple tests to spot check it

--- FractionalMathLib.Tests/TestFile.cs
+++ FractionalMathLib.Tests/TestFile.cs
@@ -1,13 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FractionalMathLib.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class TestFile
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
     }
 }
