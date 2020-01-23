81db529c88ab9c662c29b94e2e6efe03ac322fba
Quinn Gil <Fyzxs@users.noreply.github.com>
2020-01-22 18:03:11 -0800

    Added some comments to the ONLY public class in the library

--- FractionalMathLib/ProcessRequest.cs
+++ FractionalMathLib/ProcessRequest.cs
@@ -5,6 +5,11 @@ using FractionalMathLib.Results.Strings;
 
 namespace FractionalMathLib
 {
+    /* Note: A common feature of the MicroObjects (https://quinngil.com/uobjects/) style of development; there are very few classes exposed to consumers.
+        In this project, The contents of this file is the only one that outside consumers can see.
+        All of the functionality accessed through a single interface... not a lot of questions on how to use the functionality.
+    */
+
     /// <summary>
     /// The narrow interface to the deep functionality of the Fractional Math!
     /// </summary>
